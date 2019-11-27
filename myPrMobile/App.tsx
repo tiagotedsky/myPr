72fcf89c3d30542fac5113f1bec475240798ca29
TiagoSaAdvizr <Tiago.sa@advizr.co>
2019-11-27 22:31:48 +0000

    Redirect to app

--- myPrMobile/App.tsx
+++ myPrMobile/App.tsx
@@ -1,67 +1,82 @@
-import React, { useState} from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
-import { Linking } from 'expo';
-import * as WebBrowser from 'expo-web-browser';
+import React, { useState } from "react";
+import { StyleSheet, Text, View, Button } from "react-native";
+import { Linking } from "expo";
+import * as WebBrowser from "expo-web-browser";
 
 interface AuthResult {
-  type?: string,
-};
+  type?: string;
+}
 
 const handleRedirect = async event => {
+  console.log('Event ', event);
   WebBrowser.dismissBrowser();
 };
 
 const addLinkingListener = () => {
-  Linking.addEventListener('url', handleRedirect)
+  Linking.addEventListener("url", handleRedirect);
 };
 const removeLinkingListener = () => {
-  Linking.removeEventListener('url', handleRedirect)
-}
+  Linking.removeEventListener("url", handleRedirect);
+};
 
 const App = () => {
-
   const [authResult, setAuthResults] = useState<AuthResult>({});
-  
-  const handleOAuthLogin = async (event) => {
+
+  const handleOAuthLogin = async event => {
     const redirectUrl = await Linking.getInitialURL();
-
-    console.log('redirectUrl ----> ', redirectUrl);
-
     const authUrl = `https://192.168.1.69:3003/login/github`;
-    console.log('authUrl ', authUrl);
+
+    console.log("redirectUrl ----> ", redirectUrl);
+
     addLinkingListener();
-  
+
     try {
-      const authResult = await WebBrowser.openAuthSessionAsync(authUrl, redirectUrl)
-        console.log('authResult', authResult);
-        await setAuthResults(authResult)
-       } catch (err) {
-         console.log('ERROR:', err)
-       }
-    removeLinkingListener()
-   }  
+      const authResult = await WebBrowser.openBrowserAsync(
+        authUrl,
+      );
+      console.log("authResult", authResult);
+      await setAuthResults(authResult);
+    } catch (err) {
+      console.log("ERROR:", err);
+    }
+    removeLinkingListener();
+  };
+
+  const handleRequest = async () => {
+    try {
+      const response = await fetch("http://192.168.1.69:3004/getUserName");
+      const responseJson = await response.json();
+      console.log(responseJson);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <View style={styles.container}>
-      {authResult.type && authResult.type === 'success' ? 
-        (<Text>Hello there, user</Text>) 
-        :
-        (<>
-          <Text>Hello world</Text>
-          <Button title="Login with GitHub" onPress={handleOAuthLogin}/>
-        </>)
-      }
+      {authResult.type && authResult.type === "success" ? (
+        <>
+          <Text>Hello there, user</Text>
+          <Button title="Get some info" onPress={handleRequest} />
+        </>
+      ) : (
+        <>
+          <Text>Login</Text>
+          <Button title="Get some info" onPress={handleRequest} />
+          <Button title="Login with GitHub" onPress={handleOAuthLogin} />
+        </>
+      )}
     </View>
-  )
-}
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });
 
 export default App;
