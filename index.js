052f9c47553c9e5173b517ab7cf3301efe897c5e
TiagoSaAdvizr <Tiago.sa@advizr.co>
2019-12-03 23:22:51 +0000

    Get users repos

--- index.js
+++ index.js
@@ -75,6 +75,7 @@ passport.deserializeUser((obj, done) => {
 const app = express();
 const https = require("https");
 const fs = require("fs");
+const request = require('request');
 
 // Configure view engine to render EJS templates.
 app.set("views", __dirname + "/views");
@@ -137,8 +138,6 @@ app.get(
   }
 );
 
-// TEST ENDPOINTS
-
 app.get("/getUserInfo/:id", (req, res) => {  
   const userInfo = db.get(`users[${req.params.id}]`).write();
 
@@ -150,10 +149,41 @@ app.get("/getUserInfo/:id", (req, res) => {
     ...userData
   } = userInfo;
 
-  console.log('userInfo ', userInfo);
-
   res.json(userData);
 });
+
+app.get("/testing/:id", (req, res) => {
+  const userInfo = db.get(`users[${req.params.id}]`).write();
+  const { accessToken } = userInfo;
+
+  const options = {
+    url: 'https://api.github.com/user/repos',
+    headers: {
+      'User-Agent': 'request',
+      'Authorization': `token ${accessToken}`,
+    }
+  };
+
+  request(options, (error, response, body) => {
+  console.error('error:', error); // Print the error if one occurred
+  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+  // console.log('body:', body); // Print the HTML for the Google homepage.
+
+
+  console.log(JSON.parse(body));
+
+});
+
+
+
+  console.log('hello world');
+  
+  res.json({});
+  
+});
+
+
+
 
 https
   .createServer(
